9b0387efbe86d7f366cf2fce5cbd5abe7281ae8e
Kristijan Najdovski <kristijannds@gmail.com>
2021-01-10 22:20:49 +0100

    first commit

---
+++ inc/head.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Portfolio</title>
+
+    <!-- Meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
---
+++ inc/navbar.php
@@ -0,0 +1,6 @@
+<!-- Navbar -->
+<div class="container-fluid">
+  <nav class="navbar navbar-light">
+    <a class="navbar-brand text-primary" href="index.html">Navbar</a>
+  </nav>
+</div>
